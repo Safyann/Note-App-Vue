c9d8e065ac7dc939e57df415fe16ea38326dafa2
Alena Safyannikova <alena_safyanniko@mail.ru>
2019-12-09 22:48:27 +0500

    add remove and toogle

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,62 @@
     <div class="wrapper-content">
       <section>
         <div class="container">
-          <h1>{{ title }}</h1>
-
+          <!-- message -->
           <message v-if="message" :message="message" />
 
+          <!-- new note -->
           <newNote :note="note" @addNote="addNote" />
 
-          <notes :notes="notes" />
+          <!-- title -->
+          <div class="note-header">
+            <h1>{{ title }}</h1>
+            <div class="icons">
+              <svg
+                :class="{active: grid}"
+                @click="grid = true"
+                style="cursor:pointer"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <rect x="3" y="3" width="7" height="7" />
+                <rect x="14" y="3" width="7" height="7" />
+                <rect x="14" y="14" width="7" height="7" />
+                <rect x="3" y="14" width="7" height="7" />
+              </svg>
+
+              <svg
+                :class="{active: !grid}"
+                @click="grid = false"
+                style="cursor:pointer"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <line x1="8" y1="6" x2="21" y2="6" />
+                <line x1="8" y1="12" x2="21" y2="12" />
+                <line x1="8" y1="18" x2="21" y2="18" />
+                <line x1="3" y1="6" x2="3" y2="6" />
+                <line x1="3" y1="12" x2="3" y2="12" />
+                <line x1="3" y1="18" x2="3" y2="18" />
+              </svg>
+            </div>
+          </div>
+
+          <!-- note list -->
+          <notes :notes="notes" :grid="grid" @remove="removeNote" />
         </div>
       </section>
     </div>
@@ -31,6 +80,7 @@ export default {
     return {
       title: "Notes App",
       message: null,
+      grid: true,
       note: {
         title: "",
         descr: ""
@@ -73,6 +123,9 @@ export default {
       this.message = null;
       this.note.title = "";
       this.note.descr = "";
+    },
+    removeNote(index) {
+      this.notes.splice(index, 1);
     }
   }
 };
--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,9 +1,10 @@
 <template>
   <!-- note list -->
   <div class="notes">
-    <div class="note" v-for="(note, index) in notes" :key="index">
-      <div class="note-header">
+    <div class="note" :class="{full: !grid}" v-for="(note, index) in notes" :key="index">
+      <div class="note-header" :class="{full: !grid}">
         <p>{{ note.title }}</p>
+        <p style="cursor:pointer" @click="removeNote(index)">X</p>
       </div>
 
       <div class="note-body">
@@ -20,6 +21,15 @@ export default {
     notes: {
       type: Array,
       required: true
+    },
+    grid: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    removeNote(index) {
+      this.$emit("remove", index);
     }
   }
 };
@@ -39,12 +49,49 @@ export default {
   padding: 18px 20px;
   margin-bottom: 20px;
   background-color: #ffffff;
+  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
+  box-shadow: 0 30px 30px rgba($color: #000000, $alpha: 0.02);
+  &:hover {
+    box-shadow: 0 30px 30px rgba($color: #000000, $alpha: 0.04);
+    transform: translate(0, -6px);
+    transition-delay: 0s;
+  }
+  &.full {
+    width: 100%;
+    text-align: center;
+  }
 }
 
 .note-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  h1 {
+    font-size: 32px;
+  }
   p {
     color: #494ce8;
     font-size: 22px;
+  }
+  svg {
+    margin-right: 12px;
+    color: #999;
+    &:last-child {
+      margin-right: 0;
+    }
+
+    &.active {
+      color: #494ce8;
+    }
+  }
+  &.full {
+    justify-content: center;
+    p {
+      margin-right: 16px;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
   }
 }
 
